01ac4041721cd70fbdd072704f807731084a2ce6
Joe Grace <joeggrace@gmail.com>
2020-07-19 14:48:42 -0400

    Changed to single Coin Class

--- src/main/java/com/fizzbuzzcola/vendingmachine/Coin.java
+++ src/main/java/com/fizzbuzzcola/vendingmachine/Coin.java
@@ -1,4 +1,4 @@
 package com.fizzbuzzcola.vendingmachine;
 
-public class CoinValue {
+public class Coin {
 }
---
+++ src/test/java/com/fizzbuzzcola/vendingmachine/CoinTest.java
@@ -0,0 +1,10 @@
+package com.fizzbuzzcola.vendingmachine;
+
+import org.junit.jupiter.api.Test;
+
+public class CoinTest {
+    @Test
+    public void coinsShouldExist(){
+        Coin underTest;
+    }
+}
