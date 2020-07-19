2c2af8c9132d1963f7fe784d3dd331b26d1fc1e7
Joe Grace <joeggrace@gmail.com>
2020-07-19 14:19:14 -0400

    Made Coin Value Class
    Made Coin value test class and used first test to make Coin value class

---
+++ src/test/java/com/fizzbuzzcola/vendingmachine/CoinValueTest.java
@@ -0,0 +1,11 @@
+package com.fizzbuzzcola.vendingmachine;
+
+import org.junit.jupiter.api.Test;
+
+public class CoinValueTest {
+
+    @Test
+    public void coinValueClassShouldExist(){
+        CoinValue undertest;
+    }
+}
