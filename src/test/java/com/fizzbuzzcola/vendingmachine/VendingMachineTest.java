27d4bc8a34caae1d3b3611fd82025ede4ff443cc
Sara Thornsberry <sarathornsberry@saras-mbp.home>
2020-07-24 13:06:17 -0400

    starting over; made first test to create vending machine class

---
+++ src/test/java/com/fizzbuzzcola/vendingmachine/VendingMachineTest.java
@@ -0,0 +1,10 @@
+package com.fizzbuzzcola.vendingmachine;
+
+import org.junit.jupiter.api.Test;
+
+public class VendingMachineTest {
+    @Test
+    public void vendingMachineShouldExist() {
+        VendingMachine underTest = new VendingMachine();
+    }
+}
