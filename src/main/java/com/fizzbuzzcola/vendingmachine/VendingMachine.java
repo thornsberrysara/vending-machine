834c9815a1b110f7231106e06e2e1a4105eca6ba
Sara Thornsberry <sarathornsberry@saras-mbp.home>
2020-07-24 13:13:21 -0400

    test for if there's no coins in the machine, it returns insert coin

--- src/main/java/com/fizzbuzzcola/vendingmachine/VendingMachine.java
+++ src/main/java/com/fizzbuzzcola/vendingmachine/VendingMachine.java
@@ -1,4 +1,7 @@
 package com.fizzbuzzcola.vendingmachine;
 
 public class VendingMachine {
+    public String displayStatus() {
+        return "Insert Coin";
+    }
 }
--- src/test/java/com/fizzbuzzcola/vendingmachine/VendingMachineTest.java
+++ src/test/java/com/fizzbuzzcola/vendingmachine/VendingMachineTest.java
@@ -2,9 +2,18 @@ package com.fizzbuzzcola.vendingmachine;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class VendingMachineTest {
     @Test
     public void vendingMachineShouldExist() {
         VendingMachine underTest = new VendingMachine();
+    }
+
+    @Test
+    public void noCoinsInMachineShouldReturnInsertCoin() {
+        VendingMachine underTest = new VendingMachine();
+        String displayText = underTest.displayStatus();
+        assertThat(displayText).isEqualTo("Insert Coin");
     }
 }
