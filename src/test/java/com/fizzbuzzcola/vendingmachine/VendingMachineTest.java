ab4797cfaa04a1ccf16c6a9df0c4913af60fc913
Sara Thornsberry <sarathornsberry@saras-mbp.home>
2020-07-24 13:19:17 -0400

    refactoring tests

--- src/test/java/com/fizzbuzzcola/vendingmachine/VendingMachineTest.java
+++ src/test/java/com/fizzbuzzcola/vendingmachine/VendingMachineTest.java
@@ -1,19 +1,27 @@
 package com.fizzbuzzcola.vendingmachine;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class VendingMachineTest {
-    @Test
-    public void vendingMachineShouldExist() {
-        VendingMachine underTest = new VendingMachine();
+
+    private VendingMachine underTest;
+
+    @BeforeEach
+    void setup() {
+         underTest = new VendingMachine();
     }
 
     @Test
     public void noCoinsInMachineShouldReturnInsertCoin() {
-        VendingMachine underTest = new VendingMachine();
         String displayText = underTest.displayStatus();
         assertThat(displayText).isEqualTo("Insert Coin");
+    }
+
+    @Test
+    public void whenValidCoinInsertedDisplayUpdates() {
+
     }
 }
